ee6a724bfd970f64b8108eec9a65b7dea4361ee5
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-24 14:29:32 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,45 +1,12 @@
-import { useState } from "react";
-import css from "./App.module.css";
-
-// state isolation
-const ClickCounter = ({ value, onCount }) => {
-  return <button onClick={onCount}>Clicks: {value}</button>;
-};
+// import StatePractice from "./StatePractice";
+import Reader from "./Reader";
+import articles from "./data/articles.json";
 
 export default function App() {
-  const [clicks, setClicks] = useState(0); // 1. hook
-  const [values, setValues] = useState({
-    a: 1,
-    b: 2,
-    c: 3,
-  });
-
-  const handleClick = () => {
-    setClicks(clicks + 1);
-  };
-
-  const handleReset = () => {
-    setClicks(0);
-  };
-
-  const handleChange = () => {
-    setValues({
-      ...values,
-      b: 5,
-    });
-  };
-
   return (
-    <div className={css.container}>
-      <h1>State in React</h1>
-
-      <hr />
-      <button onClick={handleChange}>Change values</button>
-
-      <ClickCounter value={clicks} onCount={handleClick} />
-      <ClickCounter value={clicks} onCount={handleClick} />
-      <ClickCounter value={clicks} onCount={handleClick} />
-      <button onClick={handleReset}>Reset</button>
+    <div>
+      <Reader articles={articles} />
+      {/* <StatePractice /> */}
     </div>
   );
 }
