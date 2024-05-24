20027021a5d4747adef262232395204218f49371
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-24 15:13:24 +0300

    update

--- src/components/Text/Text.jsx
+++ src/components/Text/Text.jsx
@@ -1,3 +1,23 @@
+import { useState } from "react";
+
 export default function Text() {
-  return <div></div>;
+  const [isVisible, setisVisible] = useState(false);
+
+  const handleToggle = () => {
+    setisVisible(!isVisible);
+  };
+
+  return (
+    <div>
+      <button onClick={handleToggle}>{isVisible ? "Hide" : "Show"}</button>
+      {isVisible && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit sequi
+          aliquid ratione laboriosam sint labore nostrum, commodi placeat
+          necessitatibus et similique, error, modi rem repellat quas quaerat
+          pariatur esse dolores.
+        </p>
+      )}
+    </div>
+  );
 }
