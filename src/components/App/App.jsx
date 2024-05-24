766ff3754e294183b1778719088170cc15f9fe7c
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-24 20:46:28 +0300

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,26 @@
+// import { useState } from "react";
 // import StatePractice from "./StatePractice";
 // import Reader from "../Reader/Reader";
 // import Text from "../Text/Text";
 // import articles from "../articles.json";
 import ClickTracker from "../ClickTracker/ClickTracker";
 
+// import Timer from "../Timer/Timer";
+
 export default function App() {
+  // const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       {/* <Reader articles={articles} /> */}
       {/* <Text /> */}
       {/* <StatePractice /> */}
       <ClickTracker />
+
+      {/* <button onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "Close" : "Open"}
+      </button> */}
+
+      {/* {isOpen && <Timer />} */}
     </div>
   );
 }
