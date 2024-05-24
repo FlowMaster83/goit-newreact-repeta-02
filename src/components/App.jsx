ea9ed6c599da225c017d814323a9a1e7f85f2885
Serhii Kolesnyk <110739720+FlowMaster83@users.noreply.github.com>
2024-05-24 12:41:17 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import css from "./App.module.css";
 
+// state isolation
+const ClickCounter = ({ value, onCount }) => {
+  return <button onClick={onCount}>Clicks: {value}</button>;
+};
+
 export default function App() {
+  const [clicks, setClicks] = useState(0); // 1. hook
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  }; // 2. state function
+
   return (
     <div className={css.container}>
       <h1>State in React</h1>
+
+      <ClickCounter value={clicks} onCount={handleClick} />
+      <ClickCounter value={clicks} onCount={handleClick} />
     </div>
   );
 }
